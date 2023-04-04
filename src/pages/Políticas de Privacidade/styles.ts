ea559127dc7927099b10c28a0ded79aa93b9b75b
Gui027 <guilhermerg1234@gmail.com>
2023-04-04 16:24:05 -0300

    editado

--- src/pages/Políticas de Privacidade/styles.ts
+++ src/pages/Políticas de Privacidade/styles.ts
@@ -1,30 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  /* width: 100%; */
-  /* margin: 0px 18px;
-  padding: 0px 20px; */
+display: flex;
+  width: 100%;
+   margin: 0px 18px;
+  padding: 0px 20px; 
   overflow-x: hidden;
   background-color: #fff;
+  flex-direction: column;
+//   align-items: center;
+  
 
-  main {
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-  }
-
-.button {
-  margin-left: -60%;
-  text-align: center;
-  margin-bottom: 80px;
-}
 `
 
 export const Image = styled.div`
 div {
     display:flex;
-    justify-content: center;
+    // justify-content: center;
     align-items: center;
 
 }
@@ -34,10 +26,33 @@ div {
     // background-color:red;
     width: 50px;
     margin-top: 65px;
+    margin-bottom: 15px;
     align-items: center;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    align-self: center;
   }
+`
+
+export const H1 = styled.div`
+h1 {
+    // background-color: red;
+    display:flex;
+    // align-items: center;
+// align-content: center;
+justify-content: center;
+
+}
+display: flex;
+font-size: 10px;
+width: 550px;
+// align-items: center;
+align-content: center;
+`
+
+export const H3 = styled.div`
+font-size: 12px;
+margin-top: 8px;
+margin-bottom: 7px;
+`
+export const H4 = styled.div`
+font-size: 20px;
+margin-top: 20px;
 `
