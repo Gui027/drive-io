88b36887acd80075b9375beabd1c19f37f5633f2
Gui027 <guilhermerg1234@gmail.com>
2023-04-05 22:46:37 -0300

    route 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import PoliticaPrivacidade from './pages/Políticas de Privacidade';
@@ -13,7 +13,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/politicas" element={<PoliticaPrivacidade />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/404" element={<PageNotFound />} />
+        <Route path="*" element={<Navigate to="/404" />} />
       </Routes>
     </Router>
   </React.StrictMode>
